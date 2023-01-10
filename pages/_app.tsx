55dadd151442f1b22ac4d7d9ae99b1c9918ff3c7
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-01-09 19:29:18 -0500

    Added redux support and material UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/global.css";
 import { ContextProvider } from "../contexts/SocketContext/SocketContext";
+import { Provider } from "react-redux";
+import { store } from "../services/store";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ContextProvider>
-      <Component {...pageProps} />
-    </ContextProvider>
+    <Provider store={store}>
+      <ContextProvider>
+        <Component {...pageProps} />
+      </ContextProvider>
+    </Provider>
   );
 }
 
