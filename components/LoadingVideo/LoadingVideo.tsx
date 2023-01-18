4a988a4fc96987a23b718c1b5f2f0443555044c2
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-01-17 20:28:05 -0500

    Fix styling and add a delay

--- components/LoadingVideo/LoadingVideo.tsx
+++ components/LoadingVideo/LoadingVideo.tsx
@@ -2,7 +2,7 @@ import { Container, CircularProgress } from "@mui/material"
 
 const LoadingVideo: React.FC<{}> = () => {
     return (
-        <Container disableGutters className="bg-black flex-1 relative overflow-hidden flex justify-center items-center">
+        <Container disableGutters sx={{ display: "flex", justifyContent: "center", alignItems: "center" }} className="bg-black flex-1 relative overflow-hidden">
             <CircularProgress color="primary" />
         </Container>
     );
