48c00ca1e18346863df2748ff0328d0ec515e939
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-01-22 10:49:28 -0500

    removed fetch

--- services/modules/userSlice.ts
+++ services/modules/userSlice.ts
@@ -33,32 +33,6 @@ const initialState: IUserReducer = {
     }
 };
 
-//Thunks
-export const createTempUser: any = createAsyncThunk("user/createTemp", async (data: any) => {
-    try {
-        const response = await fetch(serverURL + "/api/temp_user", {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                username: data.username,
-                socketID: data.socketID,
-                preference: data.preference,
-            }),
-        });
-        if (!response.ok) {
-            const errData = await response.json();
-            return { err: errData.error };
-        } else {
-            const resData = await response.json();
-            return resData;
-        }
-    } catch (err) {
-        throw err;
-    }
-});
 
 const userSlice = createSlice({
     name: 'user',
@@ -83,17 +57,7 @@ const userSlice = createSlice({
         }
     },
     extraReducers: (builder) => {
-        builder.addCase(createTempUser.fulfilled, (state, action: { payload: IMongoTempUser }) => {
-            //If possible fix the socket duplication in the future
-            if (action.payload.err === apiErrors.socketIdExsists) return;
-            console.log("setting temp user reducer to", action.payload);
-            const { username, preference, _id, avatar } = action.payload;
-            state.username = username;
-            state.preference = preference;
-            state.avatar.bg = avatar.bg;
-            state.avatar.display = avatar.display;
-            state._id = _id
-        });
+
     }
 })
 
