0f8e20abb2d9a77f8c01655f2d8a5eadbaa7ecca
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-01-23 21:17:01 -0500

    Finished user dao

--- server/User/daos/user.dao.ts
+++ server/User/daos/user.dao.ts
@@ -1,5 +1,81 @@
-class UsersDao {
+import mongooseService from "../../common/services/mongoose.service";
+import { CreateUserDto, PatchUserDto, PutUserDto } from "../dto/user.dto";
 
+class UsersDao {
+    Schema = mongooseService.mongoose.Schema;
+    userSchema = new this.Schema({
+        username: String,
+        birthday: { type: Date },
+        gender: { type: String, enum: ["m", "f", "o"] },
+        preference: { type: String, enum: ["m", "f", "o"] },
+        picture: { type: String, required: false, default: "" },
+        auth0: {
+            created_at: String,
+            email: String,
+            email_verified: Boolean,
+            identities: [
+                {
+                    connection: String,
+                    provider: String,
+                    user_id: String,
+                    isSocial: Boolean,
+                }
+            ],
+            name: String,
+            nickname: String,
+            picture: String,
+            updated_at: String,
+            user_id: String,
+            last_ip: String,
+            last_login: String,
+            logins_count: Number,
+        },
+    });
+
+    User = mongooseService.mongoose.model("Users", this.userSchema);
+
+    constructor() { console.log("Initializing User Schema") }
+
+    async addUser(userFields: CreateUserDto) {
+        const user = new this.User({
+            ...userFields,
+        });
+        const newUser = await user.save().catch(err => err);
+        return newUser._id;
+    }
+
+    async getUserByAuth0Email(email: string) {
+        return this.User.findOne({ "auth0.email": email }).exec();
+    }
+
+    async getUserById(userId: string) {
+        // return this.User.findOne({ _id: userId }).populate('users').exec();
+        return this.User.findOne({ _id: userId }).exec();
+    }
+
+    async getUsers(limit = 25, page = 0) {
+        return this.User.find()
+            .limit(limit)
+            .skip(limit * page)
+            .exec();
+    }
+
+    async updateUserById(
+        userId: string,
+        userFields: PatchUserDto | PutUserDto
+    ) {
+        const existingUser = await this.User.findOneAndUpdate(
+            { _id: userId },
+            { $set: userFields },
+            { new: true }
+        ).exec();
+
+        return existingUser;
+    }
+
+    async removeUserById(userId: string) {
+        return this.User.deleteOne({ _id: userId }).exec();
+    }
 };
 
 export default new UsersDao;
--- server/User/dto/user.dto.ts
+++ server/User/dto/user.dto.ts
@@ -1,4 +1,61 @@
 export interface CreateUserDto {
-    _id: string;
-    email: string;
+    _id?: string;
+    username?: String,
+    birthday?: { type: Date },
+    gender?: { type: String, enum: ["m", "f", "o"] },
+    preference?: { type: String, enum: ["m", "f", "o"] },
+    picture?: { type: String, required: false, default: "" },
+    auth0: {
+        created_at: string,
+        email: string,
+        email_verified: boolean,
+        identities: [
+            {
+                connection: string,
+                provider: string,
+                user_id: string,
+                isSocial: boolean,
+            }
+        ],
+        name: string,
+        nickname: string,
+        picture: string,
+        updated_at: string,
+        user_id: string,
+        last_ip: string,
+        last_login: string,
+        logins_count: number,
+    }
 }
+
+export interface PutUserDto {
+    _id: string;
+    username: String,
+    birthday: { type: Date },
+    gender: { type: String, enum: ["m", "f", "o"] },
+    preference: { type: String, enum: ["m", "f", "o"] },
+    picture: { type: String, required: false, default: "" },
+    auth0: {
+        created_at: string,
+        email: string,
+        email_verified: boolean,
+        identities: [
+            {
+                connection: string,
+                provider: string,
+                user_id: string,
+                isSocial: boolean,
+            }
+        ],
+        name: string,
+        nickname: string,
+        picture: string,
+        updated_at: string,
+        user_id: string,
+        last_ip: string,
+        last_login: string,
+        logins_count: number,
+    }
+}
+
+export interface PatchUserDto extends Partial<PutUserDto> { }
