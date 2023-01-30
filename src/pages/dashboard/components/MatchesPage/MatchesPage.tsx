cf70e248f24b14e9a123b60a2b6537395c9ec36e
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-01-30 13:50:19 -0500

    Added front end for matches and messages

--- src/pages/dashboard/components/MatchesPage/MatchesPage.tsx
+++ src/pages/dashboard/components/MatchesPage/MatchesPage.tsx
@@ -1,65 +1,71 @@
-import * as React from 'react';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import { useState, useEffect } from 'react';
 import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import { useSelector } from 'react-redux';
+import { IUserReducer } from '../../../../services/modules/User/userSlice';
+import { IReducer } from '../../../../services/store';
+import { Avatar, List, ListItemButton, ListItemAvatar, ListItemText } from "@mui/material"
+import Stack from '@mui/material/Stack';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: number;
-    value: number;
-}
-
-function TabPanel(props: TabPanelProps) {
-    const { children, value, index, ...other } = props;
-
-    return (
-        <div
-            className='h-full border-2'
-            role="tabpanel"
-            hidden={value !== index}
-            id={`simple-tabpanel-${index}`}
-            aria-labelledby={`simple-tab-${index}`}
-            {...other}
-        >
-            {value === index && (
-                children
-            )}
-        </div>
-    );
-}
-
-function a11yProps(index: number) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
-}
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+    boxShadow: "none",
+}));
 
 const MatchesPage = () => {
-    const [value, setValue] = React.useState(0);
+    const userReducer: IUserReducer = useSelector((state: IReducer) => state.user);
+    const [value, setValue] = useState(0);
+
+    useEffect(() => {
+        console.log("my reducer", userReducer);
+    }, [userReducer]);
 
     const handleChange = (event: React.SyntheticEvent, newValue: number) => {
         setValue(newValue);
     };
 
-    return (
-        // <Box sx={{ width: '100%', height: "100%" }}>
-        //     <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        //         <Tabs variant='fullWidth' indicatorColor='secondary' textColor='secondary' value={value} onChange={handleChange} aria-label="basic tabs example">
-        //             <Tab label="Matches" {...a11yProps(0)} />
-        //             <Tab label="Messages" {...a11yProps(1)} />
-        //         </Tabs>
-        //     </Box>
-        //     <TabPanel value={value} index={0}>
-        //         Item One
-        //     </TabPanel>
-        //     <TabPanel value={value} index={1}>
+    return userReducer.matches.length > 0 ? (
+        <div className='flex items-center h-full flex-col w-full border-2 p-5'>
+            <div className='w-full flex flex-col mb-5'>
+                <Typography variant='h6'>Matches</Typography>
+                <Stack
+                    direction="row"
+                    sx={{ overflowX: "scroll" }}
+                >
+                    {userReducer.matches.map((match, index) => {
+                        return (
+                            <Item key={index + "matchesKey"}>
+                                <Avatar sx={{ width: 56, height: 56 }} alt={`${match.username} avatar`} src={match.picture} />
+                                <Typography marginTop={0.5} fontWeight={"bold"} variant='body2'>{match.username}</Typography>
+                            </Item>
+                        );
+                    })}
+                </Stack>
+            </div>
+            <div className='w-full flex flex-col mb-5'>
+                <Typography variant='h6'>Messages</Typography>
+                <List>
+                    <ListItemButton>
+                        <ListItemAvatar>
+                            <Avatar alt="Profile Picture" src={undefined} />
+                        </ListItemAvatar>
+                        <ListItemText
+                            primary={"Brunch this week?"}
+                            secondary={"I'll be in the neighbourhood this week. Let's grab a bite to eat"}
+                        />
+                    </ListItemButton>
+                </List>
+            </div>
+        </div>
+    ) : (
         <div className='flex justify-center items-center h-full'>
             <Typography variant='h4'>No Matches</Typography>
         </div>
-        //     </TabPanel>
-        // </Box>
     );
 }
 
