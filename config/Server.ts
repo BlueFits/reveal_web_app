2663b0f3abe0c55f8c8a646b75fc9c51478c21f1
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-02-04 17:34:51 -0500

    Update Server.ts

--- config/Server.ts
+++ config/Server.ts
@@ -8,5 +8,5 @@ const apis = {};
 apis[status.prod] = "https://reaveal-web-app.onrender.com";
 apis[status.development] = "http://localhost:3000";
 
-export const currentENV = status.development;
-export const serverURL = process.env.NODE_ENV === "production" ? apis[status.prod] : apis[currentENV];
+export const currentENV = process.env.NODE_ENV === "production" ? status.prod : status.development;
+export const serverURL = process.env.NODE_ENV === "production" ? apis[status.prod] : apis[status.development];
