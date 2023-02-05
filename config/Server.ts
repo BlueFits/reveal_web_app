5121958d81d621cdc437594d79173fd968d65f7f
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-02-04 22:39:37 -0500

    Update Server.ts

--- config/Server.ts
+++ config/Server.ts
@@ -1,11 +1,13 @@
 export enum status {
     development = "development",
-    prod = "render",
+    prod = "reveal-app",
+    render = "render",
 }
 
 const apis = {};
 
-apis[status.prod] = "https://reaveal-web-app.onrender.com";
+apis[status.prod] = "https://www.reveal-app.site";
+apis[status.render] = "https://reaveal-web-app.onrender.com/";
 apis[status.development] = "http://localhost:3000";
 
 export const currentENV = process.env.NODE_ENV === "production" ? status.prod : status.development;
