489c35b61dcaefc605d3074a57269c2488334355
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-02-05 12:25:48 -0500

    Fixed homepage scroll x

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Index = () => {
     );
 
     return (
-        <div>
+        <div className="overflow-x-hidden">
             <div
                 style={{
                     backgroundImage: "url('https://images.unsplash.com/photo-1611872687047-5e5cabc8e1bd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=986&q=80')"
