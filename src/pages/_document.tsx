9025ab6a15e172e723b2602e3dee5ca47513e3ed
bluefits <44682470+BlueFits@users.noreply.github.com>
2023-02-08 15:29:55 -0500

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,6 @@ class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-W2YP40HSXZ')
             `,
             }}
           />
